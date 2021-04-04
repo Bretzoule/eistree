1157a6df6d460eb29bfa171e8f6e805133621630
Celine <celine.guillemin@eisti.fr>
2021-04-04 21:44:32 +0200

    modifs page produit

--- fichesproduits/article.php
+++ fichesproduits/article.php
@@ -63,11 +63,12 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
                 </div>
             </div>
             <div class="entretienProduit">
-                <strong>Arrosage :<?php if(isset($produit['arrosage'])) { echo $produit['arrosage'];} ?></strong> <br>
-                <strong>Luminosité :<?php if(isset($produit['luminosité'])) { echo $produit['luminosité'];} ?></strong> <br>
-                <strong>Température :<?php if(isset($produit['température'])) { echo $produit['température'];} ?></strong> <br>
-                <strong>Feuillage :<?php if(isset($produit['feuillage'])) { echo $produit['feuillage'];} ?></strong> <br>
-                <strong>Floraison :<?php if(isset($produit['floraison'])) { echo $produit['floraison'];} ?></strong> <br>
+                <strong><?php if(isset($produit['arrosage'])) { echo 'Arrosage : ' . $produit['arrosage'];} ?></strong> <br>
+                <strong><?php if(isset($produit['luminosité'])) { echo 'Luminosité : ' . $produit['luminosité'];} ?></strong> <br>
+                <strong><?php if(isset($produit['température'])) { echo 'Température : ' . $produit['température'];} ?></strong> <br>
+                <strong><?php if(isset($produit['feuillage'])) { echo 'Feuillage : ' . $produit['feuillage'];} ?></strong> <br>
+                <strong><?php if(isset($produit['hauteur'])) { echo 'Hauteur : ' . $produit['hauteur'];} ?></strong> <br>
+                <strong><?php if(isset($produit['floraison'])) { echo 'Floraison : ' . $produit['floraison'];} ?></strong> <br>
             </div>
         </div>
     </div>
