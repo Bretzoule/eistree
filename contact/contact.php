30e9acac78485a8ec2f41811023e9556ff74348c
Celine <celine.guillemin@eisti.fr>
2021-04-04 19:09:33 +0200

    rajout js

--- contact/contact.php
+++ contact/contact.php
@@ -148,7 +148,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
     </div>
 </div>
-<!-- <script src="/js/contact.js"></script> -->
+<script src="/js/contact.js"></script>
 
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . 'includes/footer.php');
